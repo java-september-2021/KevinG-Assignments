55d24948015b87c0d770d3a43828d1b50b77846a
Kevin Gardner <emailklg2013@gmail.com>
2021-09-08 00:58:57 -0700

    homework

---
+++ 01-java-fundamentals/05-basics/BasicJavaTest.java
@@ -0,0 +1,19 @@
+public class BasicJavaTest {
+    public static void main(String[] args){
+        BasicJava test = new BasicJava();
+        // test.print255(); 
+        // test.printOdd255();  3
+        // System.out.println(test.printSum());
+        // int[] arr1 = {3,5,7,9,13,22};
+        // int[] arr2 = {-2,-12,-55,-4};
+        // test.iterateArr(arr1);
+        // test.findMax(arr1);
+        // test.findMax(arr2);
+        // System.out.println(test.arrOdd());,
+        int[] arr3 ={5,25,15};
+        int[] arr4 ={5,25,15,35,22,17,105};
+        System.out.println(test.getAverage(arr3));
+        System.out.println(test.getAverage(arr4));
+
+    }
+}
